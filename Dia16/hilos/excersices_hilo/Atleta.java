806e2979e0fef7256e4cb99056f7e0b8161ceb06
jeanFrancoGuerreroAcero <166477787+jeanFrancoGuerreroAcero@users.noreply.github.com>
2025-03-13 10:13:58 -0500

    feat :sparkles: improve in code

--- Dia16/hilos/excersices_hilo/Atleta.java
+++ Dia16/hilos/excersices_hilo/Atleta.java
@@ -1,71 +1,46 @@
 
 package excersices_hilo;
 
+import java.util.Random;
 
-public class Atleta {
-    private String atleta;
-    
-    private static double tiempoT = 0;
-    
-    public Atleta(String relevo){
-        relevo = relevo;
-    }
-    
-    public Atleta(){
-        tiempoT = 0;
-    }
-    
-    public Atleta(String atleta){
-        this.atleta = atleta; // Asignación correcta
-    }
 
-    public Atleta(){
-        tiempoT = 0;
-    }
+public class Atleta extends Thread{
+   
+    private String nombre;
 
-    public static double getTiempo(){
-        return tiempoT;
-    }
+    public Atleta(String nombre) {
+        this.nombre = nombre;
+    }   
     
     public void run() {
-        double tiempo = 0;
-        synchronized(getClass()) {
-            if (tiempo == 0) {
-                System.out.println("Comienza a correr " + getName());
-                tiempo = (Math.random() * (11 - 9) + 9); // Tiempo aleatorio entre 9 y 11 segundos
-                tiempoT += tiempo;
-            } else {
-                System.out.println("Pasa el relevo y comienza a correr " + getName());
-                tiempo = (Math.random() * (11 - 9) + 9);
-                tiempoT += tiempo;
+        synchronized (getClass()) {
+            
+            try{
+                Random random = new Random();
+                System.out.println(nombre + " Arranco");
+                int testigo = random.nextInt(9000)+2000;
+                Thread.sleep(testigo);
+                System.out.println("el " + nombre + " termino la carrera " + "el tiempo de la carrera es: " + testigo/1000);
             }
-            System.out.println("El corredor: " + getName() + " ha tardado: " + tiempo);
+            catch(InterruptedException e){
+                getClass().notifyAll();
+            }
+            getClass().notifyAll();
         }
+        
+        
     }
 
-    // Método para obtener el nombre del atleta
-    public String getName() {
-        return this.atleta;
-    }
 
-    public static void main(String[] args) throws InterruptedException {
-        // Crear instancias de Atleta
-        Atleta a1 = new Atleta("Pedro");
-        Atleta a2 = new Atleta("Pablo");
-        Atleta a3 = new Atleta("Usain");
-        Atleta a4 = new Atleta("Manolo");
-
-        // Iniciar los hilos de los atletas
-        a1.start();
-        a1.join();
-        a2.start();
-        a2.join();
-        a3.start();
-        a3.join();
-        a4.start();
-        a4.join();
-
-        // Mostrar el tiempo total al final de la carrera
-        System.out.println("Final de la carrera, tiempo total: " + Atleta.getTiempo());
+    public static void main(String[] args) {
+        Atleta at1 = new Atleta("juancho");
+        Atleta at2 = new Atleta("jose");
+        Atleta at3 = new Atleta("andrea");
+        Atleta at4 = new Atleta("camilo");
+        
+        at1.start();
+        at2.start();
+        at3.start();
+        at4.start();
     }
 }
